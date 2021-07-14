1923e6a7600a6aef14f56f02bf29bcb299ecf0d4
syaina <syainanurf@gmail.com>
2021-07-14 22:36:44 +0700

    update home layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import Header from '../components/Header';
 
 import Grid from '@material-ui/core/Grid';
+
+import Header from '../components/Header';
+import DisplaySlider from '../components/DisplaySlider';
 
 export default function Home () {
     return (
@@ -10,6 +12,7 @@ export default function Home () {
             <div className="container">
                 <Grid container spacing={3}>
                     <Grid item sm={12} md={7}>
+                        <DisplaySlider />
                     </Grid>
                     <Grid item sm={12} md={5}>
                         <h1 className="title mt-5">Tentir Aorta</h1>
@@ -21,7 +24,68 @@ export default function Home () {
                     </Grid>
                 </Grid>
             </div>
-            
+
+            <div className="d-flex">
+                <div className="container bg-dark-blue tab-style font-white">
+                    <h5 className="mt-3">Butuh tantangan?</h5>
+                    <p>Yuk jawab soal</p>
+                </div>
+                <div className="tab-like-section">
+                    
+                </div>
+            </div>
+            <div className="bg-dark-blue py-3">
+                <div className="container py-5">
+                    <div className="bg-butterscotch container-question-box">
+                        <div className="bg-white question-box">
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, molestias voluptatem. Fuga veniam ullam, suscipit adipisci iusto vel quisquam temporibus ducimus deserunt amet natus qui repellendus rem rerum animi iure!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur.</p>
+                        </div>
+                    </div>
+                    <div className="my-5">
+                        <a className="link-btn" href="/">Lihat soal lainnya</a>
+                    </div>
+
+                    <Grid container spacing={3}>
+                        <Grid item sm={12} md={3} lg={3}>
+                            <div class="card bg-white">
+                                <div class="card-img-container">
+                                    <img src="./images/image-4.jpg" alt="Bab" />
+                                </div>
+                                <h5>Bab Anatomi</h5>
+                            </div>
+                        </Grid>
+                        <Grid item sm={12} md={3} lg={3}>
+                            <div class="card bg-white">
+                                <div class="card-img-container">
+                                    <img src="./images/image-4.jpg" alt="Bab" />
+                                </div>
+                                <h5>Bab Anatomi</h5>
+                            </div>
+                        </Grid>
+                        <Grid item sm={12} md={3} lg={3}>
+                            <div class="card bg-white">
+                                <div class="card-img-container">
+                                    <img src="./images/image-4.jpg" alt="Bab" />
+                                </div>
+                                <h5>Bab Anatomi</h5>
+                            </div>
+                        </Grid>
+                        <Grid item sm={12} md={3} lg={3}>
+                            <div class="card bg-white">
+                                <div class="card-img-container">
+                                    <img src="./images/image-4.jpg" alt="Bab" />
+                                </div>
+                                <h5>Bab Anatomi</h5>
+                            </div>
+                        </Grid>
+                    </Grid>
+                </div>
+            </div>
         </div>
     );
 }
