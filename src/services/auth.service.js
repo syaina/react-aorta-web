f9f66112fc5dfcf52607aa1968625926229aa592
syaina <syainanurf@gmail.com>
2021-07-23 23:35:30 +0700

    update auth.service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,32 +1,26 @@
 import axios from "axios";
 
-const API_URL = "https://api.tentiraorta.com/";
-
 class AuthService {
-    login(email, password) {
-        return axios
-        .post(API_URL + "login", {
-            email,
-            password
-        })
-        .then(response => {
-            // if (response.data.accessToken) {
-            //   localStorage.setItem("user", JSON.stringify(response.data));
-            // }
-
-            console.log(email, password);
-            console.log(response.data)
-        });
-    }
-
-    logout() {
-        localStorage.removeItem("token");
-        localStorage.removeItem("nama");
-        localStorage.removeItem("email");
-    }
+    // login(email, password) {
+    //     return axios.post('/login', {
+    //         email: email,
+    //         password: password
+    //     })
+    //     .then(response => { 
+    //         if (response.data.result.token) {
+    //             localStorage.setItem("token", JSON.stringify(response.data.result.token));
+    //             localStorage.setItem("id_user", JSON.stringify(response.data.result.id_user));
+    //             localStorage.setItem("name", JSON.stringify(response.data.result.nama));
+    //             localStorage.setItem("email", JSON.stringify(response.data.result.email));
+    //         }
+    //     })
+    //     .catch(error => {
+    //         console.log(error.response)
+    //     });   
+    // }
 
     register(email, password, nama, asal_universitas, pekerjaan) {
-        return axios.post(API_URL + "register", {
+        return axios.post("/register", {
         email,
         password,
         nama, 
@@ -40,11 +34,22 @@ class AuthService {
     }
 
     getUser() {
-        return JSON.parse(localStorage.getItem('nama'));
+        return JSON.parse(localStorage.getItem('id_user'));
+    }
+
+    getName() {
+        return JSON.parse(localStorage.getItem('name'));
     }
 
     getEmail() {
         return JSON.parse(localStorage.getItem('email'));
+    }
+
+    logout() {
+        localStorage.removeItem("token");
+        localStorage.removeItem("id_user");
+        localStorage.removeItem("name");
+        localStorage.removeItem("email");
     }
 }
 
