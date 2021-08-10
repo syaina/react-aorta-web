70cd69aa27b22ea2bfd153834634d4696f9bb9d7
syaina <syainanurf@gmail.com>
2021-08-11 00:58:41 +0700

    add confirm dialog

---
+++ src/components/ConfirmDialog.js
@@ -0,0 +1,66 @@
+import React, {useState, useEffect} from 'react';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  Box,
+  IconButton,
+  Typography,
+} from '@material-ui/core';
+import { Close } from '@material-ui/icons';
+import create from 'zustand';
+
+const useConfirmDialogStore = create((set) => ({
+  message: '',
+  onSubmit: undefined,
+  close: () => set({ onSubmit: undefined }),
+}));
+
+const ConfirmDialog = () => {
+  // destructure the store data and functions
+ const { message, onSubmit, close } = useConfirmDialogStore();
+ return (
+    // if the onSubmit is undefined the dialog will be closed. 
+    // close() function sets the onSubmit to undefined, 
+    // so it will close the dialog, if we pass it to the onClose attribute.
+   <Dialog open={Boolean(onSubmit)} onClose={close} maxWidth="sm" fullWidth> 
+     <DialogTitle>Langkah Konfirmasi</DialogTitle>
+     <Box position="absolute" top={0} right={0}>
+       <IconButton onClick={close}>
+         <Close />
+       </IconButton>
+     </Box>
+     <DialogContent>
+       <Typography>{message}</Typography>
+     </DialogContent>
+     <DialogActions>
+       <Button color="grey" variant="" onClick={close}>
+         Kembali
+       </Button>
+       <Button
+         color="primary"
+         variant="contained"
+         onClick={() => {
+           if (onSubmit) {
+             onSubmit();
+           }
+           close();
+         }}
+       >
+         Lanjutkan
+       </Button>
+     </DialogActions>
+   </Dialog>
+ );
+};
+
+export const confirmDialog = (message, onSubmit) => {
+  useConfirmDialogStore.setState({
+    message,
+    onSubmit,
+  });
+};
+
+export default ConfirmDialog;
